a7fa76d5db373d548da6f0255379f4b2107003b2
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-05-20 03:57:00 +0600

    delete count data

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run() {
 
     })
 
+    //delete
+    app.delete('/addToy/:id',async(req,res)=>{
+      const id = req.params.id
+      const query = {_id:new ObjectId(id)}
+      const result = await AddToyCollection.deleteOne(query)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
