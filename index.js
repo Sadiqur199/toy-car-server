3e806a50c5003afafa17ea0d56730f2c974b6814
Sadiqur199 <sadiqurrhaman199@gmail.com>
2023-05-20 08:23:18 +0600

    solve the client connection

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
 
     const productCollection = client.db('ToyCars').collection('AllProducts')
     const AddToyCollection = client.db('ToyCars').collection('AddToy')
